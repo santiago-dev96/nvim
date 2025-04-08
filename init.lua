fb368d89eb5342d6da6c2ac995a4527dcf787987
Santiago Rodriguez <santo1996.29@gmail.com>
2025-04-08 17:55:50 -0400

    simplified things

--- init.lua
+++ init.lua
@@ -6,22 +6,7 @@ vim.o.expandtab = false
 -- no wrap
 vim.o.wrap = false
 
--- leader key
-vim.g.mapleader = ' '
-
--- alternate file jumping
-vim.keymap.set('n', '<leader><leader>', '<cmd>e#<cr>')
-
--- folding
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.o.foldlevel = 3
-vim.o.foldminlines = 10
-
 -- features
 vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
-
--- package manager
-require'config.lazy'
--- lua/plugins/lspconfig.lua
+++
@@ -1,8 +0,0 @@
-return {
-	'neovim/nvim-lspconfig',
-	config = function()
-		require'config.lsp.ts_ls'
-		require'config.lsp.gopls'
-		require'config.lsp.lua_ls'
-	end
-}
